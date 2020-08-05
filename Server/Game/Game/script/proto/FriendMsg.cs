730689a9f9901f70a5fe78faeef17c99a231bc3a
mogebingxue <2542028845@qq.com>
2020-08-05 22:32:03 +0800

    音量设置，退出游戏，头像更换

---
+++ Server/Game/Game/script/proto/FriendMsg.cs
@@ -0,0 +1,53 @@
+﻿
+using System.Collections.Generic;
+//获取好友列表
+public class MsgGetFriendList : MsgBase
+{
+    public MsgGetFriendList() { protoName = "MsgGetFriendList"; }
+    //服务端回
+    //好友列表
+    public List<string> friendIdList = new List<string>();
+    //0 成功 1 失败
+    public int result = 0;
+}
+
+//删除好友
+public class MsgDeleteFriend : MsgBase
+{
+    public MsgDeleteFriend() { protoName = "MsgDeleteFriend"; }
+    //客户端发
+    public string friendId = "";
+    public string id = "";
+    //服务端回
+    //0 成功 1 失败
+    public int result = 0;
+}
+
+//添加好友
+public class MsgAddFriend : MsgBase
+{
+    public MsgAddFriend() { protoName = "MsgAddFriend"; }
+
+    //客户端发 服务端回
+    public string friendId = "";
+    public string id = "";
+
+    //服务端回
+    //0 成功 1 玩家不存在 2 玩家不在线 3 已经是好友
+    public int result = 0;
+}
+
+//同意添加好友
+public class MsgAcceptAddFriend : MsgBase
+{
+    public MsgAcceptAddFriend() { protoName = "MsgAcceptAddFriend"; }
+
+    //客户端发 服务端回
+    public string friendId = "";
+    public string id = "";
+
+    //服务端回
+    public int result = 0;
+}
+
+
